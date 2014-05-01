dc44bb758e8fdbb69c21682fde17585ecd4f3f12
Jay OConnor <jay@bellycard.com>
2014-05-01 17:54:28 -0500

    do not trigger the update on a PR

--- lib/payload_processor.rb
+++ lib/payload_processor.rb
@@ -8,7 +8,7 @@ class PayloadProcessor
   def process_payload(project: nil, payload: nil)
     self.project = project
     self.payload = payload
-    add_statuses
+    add_statuses if payload.type == "pull_request"
     update_building_status
     payload_log
   end
