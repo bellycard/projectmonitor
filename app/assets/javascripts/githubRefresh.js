693229e1b0bd417c1731509f9fca483b1a5ba352
Jay OConnor <jay@bellycard.com>
2014-05-02 15:47:57 -0500

    remove constant polling to github

--- app/assets/javascripts/githubRefresh.js
+++ app/assets/javascripts/githubRefresh.js
@@ -4,37 +4,9 @@ var GithubRefresh = (function () {
 
   return {
     init : function () {
-      $githubTile = $('.github');
-
-      timeoutFunction = setTimeout(this.refresh, pollIntervalSeconds * 1000);
     },
 
     refresh : function () {
-      $.ajax({
-        url: '/github_status.json',
-        timeout: (pollIntervalSeconds - 1) * 1000,
-        success: function(response) {
-          var status = response.status;
-          if(status == 'good') {
-            $githubTile.slideUp();
-            failureCount = 0;
-          }
-          else {
-            failureCount++;
-            if (failureCount >= failureThreshold) {
-              if(status == 'unreachable') {
-                GithubRefresh.markAsUnreachable();
-              } else {
-                GithubRefresh.markAsDown();
-              }
-            }
-          }
-        },
-        error: function(x,y,z) {
-          // only display unreachable error when external service unreachable
-        }
-      });
-      timeoutFunction = setTimeout(GithubRefresh.refresh, pollIntervalSeconds * 1000);
     },
 
     cleanupTimeout : function () {
@@ -42,17 +14,9 @@ var GithubRefresh = (function () {
     },
 
     markAsUnreachable: function () {
-      $githubTile.find('a').text("GITHUB IS UNREACHABLE");
-      $githubTile.removeClass('bad');
-      $githubTile.addClass('unreachable');
-      $githubTile.slideDown();
     },
 
     markAsDown: function () {
-      $githubTile.find('a').text("GITHUB IS DOWN");
-      $githubTile.removeClass('unreachable');
-      $githubTile.addClass('bad');
-      $githubTile.slideDown();
     }
   };
 })();
